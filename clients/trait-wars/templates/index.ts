6af8bf8cdfd42af336b6b7df300acf4093201fe0
osamah <oalghnmi@gmail.com>
2026-02-05 15:14:04 +0100

    hello

--- clients/trait-wars/templates/index.ts
+++ clients/trait-wars/templates/index.ts
@@ -15,7 +15,7 @@ export { ScenarioSelector, type ScenarioSelectorProps } from './ScenarioSelector
 export { TraitWarsGame, type TraitWarsGameProps, type CombatLogEntry as GameCombatLogEntry, type GamePhase } from './TraitWarsGame';
 
 // High-fidelity hex grid template
-export { HexTraitWarsGame, type HexTraitWarsGameProps, type HexGameUnit, type GamePhase as HexGamePhase, type LogEntry } from './HexTraitWarsGame';
+export { HexTraitWarsGame, type HexTraitWarsGameProps, type HexGameUnit, type GamePhase as HexGamePhase } from './HexTraitWarsGame';
 
 // Hero and Strategic templates
 export { HeroProfileTemplate, type HeroProfileTemplateProps, type HeroData } from './HeroProfileTemplate';
