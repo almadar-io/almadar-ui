1a3741064eb07404cfc59930d1f7e1c77088483e
osamah <oalghnmi@gmail.com>
2026-02-05 04:22:35 +0100

    Fixing the issues

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -6,6 +6,47 @@ import React from 'react';
 import '../themes/index.css';
 import '../index.css';
 
+// Import TraitWarsAssetProvider for Trait Wars stories
+import { TraitWarsAssetProvider, TraitWarsAssetManifest } from '../clients/trait-wars/assets';
+
+// Asset manifest for Storybook - assets are served at root via staticDirs
+const STORYBOOK_ASSET_MANIFEST: TraitWarsAssetManifest = {
+    baseUrl: '', // Assets served at root in Storybook
+    units: {
+        hero: 'game-sprites/robots/04_hero_isometric.png',
+        caregiver: 'game-sprites/robots/03_caregiver_isometric.png',
+        explorer: 'game-sprites/robots/05_explorer_isometric.png',
+        sage: 'game-sprites/robots/10_sage_isometric.png',
+        shadowLegion: 'game-sprites/robots/iram_shadow_isometric.png',
+        emperor: 'game-sprites/robots/12_emperor_isometric.png',
+    },
+    terrain: {
+        plains: 'game-sprites/terrain/manuscript_plains.png',
+        forest: 'game-sprites/terrain/illuminated_forest.png',
+        mountain: 'game-sprites/terrain/scripture_mountains.png',
+        water: 'game-sprites/terrain/ink_water.png',
+        fortress: 'game-sprites/terrain/bone_castle.png',
+        castle: 'game-sprites/terrain/bone_castle.png',
+    },
+    ui: {
+        healthBar: 'game-sprites/ui/health_bar.png',
+        traitFrame: 'game-sprites/ui/trait_frame.png',
+        button: 'game-sprites/ui/button.png',
+        panelBg: 'game-sprites/ui/panel_bg.png',
+    },
+    spriteSheets: {
+        pixelTilemap: 'pixel-platformer/tilemap.png',
+        pixelCharacters: 'pixel-platformer/tilemap-characters.png',
+        dungeonTilemap: 'tiles/dungeon/roguelikeDungeon_transparent.png',
+    },
+    effects: {
+        attack: 'game-sprites/effects/attack.png',
+        heal: 'game-sprites/effects/heal.png',
+        defend: 'game-sprites/effects/defend.png',
+        death: 'game-sprites/effects/death.png',
+    },
+};
+
 const preview: Preview = {
     parameters: {
         controls: {
@@ -35,10 +76,13 @@ const preview: Preview = {
             defaultTheme: 'minimalist',
             attributeName: 'data-design-theme',
         }),
+        // Wrap all stories with TraitWarsAssetProvider for sprite loading
         (Story) => (
-            <div style={{ padding: '1rem' }}>
-                <Story />
-            </div>
+            <TraitWarsAssetProvider manifest={STORYBOOK_ASSET_MANIFEST}>
+                <div style={{ padding: '1rem' }}>
+                    <Story />
+                </div>
+            </TraitWarsAssetProvider>
         ),
     ],
 
